c502280470cebca5535cbbbafcb31b78ab906bdd
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-17 14:36:34 -0300

    Create authentication-service

---
+++ authentication-service/src/main/java/com/omar/hotel_reservation/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omar.hotel_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+}
