4ab57042aaecc98f6ecb4c24c2ab437ea69df6f6
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-20 11:39:50 -0300

    Create HotelMapper to handle dtos

---
+++ hotel-service/src/main/java/com/omar/hotel_reservation/mappers/HotelMapper.java
@@ -0,0 +1,31 @@
+package com.omar.hotel_reservation.mappers;
+
+import com.omar.hotel_reservation.dtos.request.HotelPutRequestDTO;
+import com.omar.hotel_reservation.dtos.request.HotelRequestDTO;
+import com.omar.hotel_reservation.entities.Hotel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HotelMapper {
+
+    public Hotel toHotel(HotelRequestDTO hotelRequestDTO){
+        return Hotel.builder()
+                .name(hotelRequestDTO.name())
+                .description(hotelRequestDTO.description())
+                .country(hotelRequestDTO.country())
+                .state(hotelRequestDTO.state())
+                .city(hotelRequestDTO.city())
+                .postalCode(hotelRequestDTO.postalCode())
+                .category(hotelRequestDTO.category())
+                .status(hotelRequestDTO.status())
+                .build();
+    }
+
+    public Hotel toHotel(HotelPutRequestDTO hotelPutRequestDTO, Hotel hotel){
+        hotel.setDescription(hotelPutRequestDTO.description());
+        hotel.setPostalCode(hotelPutRequestDTO.postalCode());
+        hotel.setCategory(hotelPutRequestDTO.category());
+        hotel.setAddress(hotelPutRequestDTO.address());
+        return hotel;
+    }
+}
