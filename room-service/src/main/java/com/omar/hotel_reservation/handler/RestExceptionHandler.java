fe6f2af2722d4ff25b436d7b1472d168131a8f49
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-20 17:29:22 -0300

    Handling custom exceptions

--- room-service/src/main/java/com/omar/hotel_reservation/handler/RestExceptionHandler.java
+++ room-service/src/main/java/com/omar/hotel_reservation/handler/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.omar.hotel_reservation.handler;
 
+import feign.FeignException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,19 @@ public class RestExceptionHandler {
                 ErrorMessage.builder()
                         .timestamp(LocalDateTime.now())
                         .title("Entity not found")
+                        .message(exception.getMessage())
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .statusCode(HttpStatus.NOT_FOUND.value())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(FeignException.class)
+    private ResponseEntity<ErrorMessage> handler(FeignException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ErrorMessage.builder()
+                        .timestamp(LocalDateTime.now())
+                        .title("Exception during fetching data from hotel-service")
                         .message(exception.getMessage())
                         .httpStatus(HttpStatus.NOT_FOUND)
                         .statusCode(HttpStatus.NOT_FOUND.value())
