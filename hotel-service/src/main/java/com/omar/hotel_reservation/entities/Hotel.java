5e92cb1746cc29562a1a73b21819474633da9368
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-20 09:18:59 -0300

    Creating hotel entity

---
+++ hotel-service/src/main/java/com/omar/hotel_reservation/entities/Hotel.java
@@ -0,0 +1,37 @@
+package com.omar.hotel_reservation.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tb_hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String address;
+
+    private String country;
+
+    private String state;
+
+    private String city;
+
+    private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+}
