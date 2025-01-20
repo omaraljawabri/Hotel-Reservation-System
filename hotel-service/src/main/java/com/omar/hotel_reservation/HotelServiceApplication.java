2aa915657757092087b69fb1692bf066bce36ac5
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-20 08:48:43 -0300

    Create hotel-service

---
+++ hotel-service/src/main/java/com/omar/hotel_reservation/HotelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omar.hotel_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceApplication.class, args);
+	}
+
+}
