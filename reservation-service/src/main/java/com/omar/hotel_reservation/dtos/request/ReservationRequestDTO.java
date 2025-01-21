8bf17f886fb80db8fba51819d5a4c3af63591a65
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-21 15:23:24 -0300

    Adding validation to ReservationRequestDTO

--- reservation-service/src/main/java/com/omar/hotel_reservation/dtos/request/ReservationRequestDTO.java
+++ reservation-service/src/main/java/com/omar/hotel_reservation/dtos/request/ReservationRequestDTO.java
@@ -1,12 +1,19 @@
 package com.omar.hotel_reservation.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record ReservationRequestDTO(
-       Long hotelId,
-       Long roomId,
-       Long userId,
-       LocalDate checkInDate,
-       LocalDate checkOutDate
+        @NotNull(message = "Hotel id is required")
+        Long hotelId,
+        @NotNull(message = "Room id is required")
+        Long roomId,
+        @NotNull(message = "User id is required")
+        Long userId,
+        @NotNull(message = "Date of check in is required")
+        LocalDate checkInDate,
+        @NotNull(message = "Date of check out is required")
+        LocalDate checkOutDate
 ) {
 }
