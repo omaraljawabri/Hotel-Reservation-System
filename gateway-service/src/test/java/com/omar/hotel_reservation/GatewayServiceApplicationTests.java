bb908a44045193216d375739018da3b249ab823a
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-22 17:28:47 -0300

    Create gateway-service

---
+++ gateway-service/src/test/java/com/omar/hotel_reservation/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.hotel_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
