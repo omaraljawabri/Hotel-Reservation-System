c84a7dae58a687d677237e042bf599a9e2d18fd7
Omar <164200540+omaraljawabri@users.noreply.github.com>
2025-01-25 16:23:17 -0300

    Adding authorization header in feign requests

--- reservation-service/src/main/java/com/omar/hotel_reservation/clients/RoomClient.java
+++ reservation-service/src/main/java/com/omar/hotel_reservation/clients/RoomClient.java
@@ -2,11 +2,9 @@ package com.omar.hotel_reservation.clients;
 
 import com.omar.hotel_reservation.dtos.request.RoomRequestDTO;
 import com.omar.hotel_reservation.dtos.response.RoomResponseDTO;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -14,12 +12,13 @@ import java.util.Optional;
         name = "room-service",
         url = "http://localhost:8080/api/v1/room"
 )
+@Headers("Authorization: {token}")
 public interface RoomClient {
 
     @GetMapping("/{id}")
-    Optional<RoomResponseDTO> findRoomById(@PathVariable("id") Long id);
+    Optional<RoomResponseDTO> findRoomById(@PathVariable("id") Long id, @RequestHeader("Authorization") String token);
 
     @PutMapping
-    RoomResponseDTO updateRoom(@RequestBody RoomRequestDTO roomRequestDTO);
+    RoomResponseDTO updateRoom(@RequestBody RoomRequestDTO roomRequestDTO, @RequestHeader("Authorization") String token);
 
 }
